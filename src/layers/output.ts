ccb2e147238670e844fd4e78b366e31dab5f2bac
Fedor Indutny <fedor@indutny.com>
2018-08-13 14:11:21 -0400

    output: tanh angle

--- src/layers/output.ts
+++ src/layers/output.ts
@@ -11,13 +11,13 @@ export class Output extends tf.layers.Layer {
         tf.split(inputs, [ 2, 2, 1, 1 ], -1);
 
     const depth = inputs.shape[inputs.shape.length - 2];
-    const anglePrior = tf.linspace(0, 0.5, depth)
+    const anglePrior = tf.linspace(0, 1 - (1 / depth), depth)
       .reshape(inputs.shape.slice(0, -2).fill(1).concat(depth)).expandDims(-1);
 
     return tf.concat([
       tf.sigmoid(center),
       tf.exp(size),
-      angle.add(anglePrior),
+      tf.tanh(angle).add(anglePrior),
       tf.sigmoid(confidence),
     ], -1);
   }
